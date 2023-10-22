9450013d1efa9d9b028caf6a5e551a20d3ad8a75
Clgoggle <misaryou.0404.0813@gmail.com>
2023-10-22 11:21:13 +0900

    バルバトス取るか悩む

---
+++ RPG/BukiList.swift
@@ -0,0 +1,20 @@
+//
+//  BukiList.swift
+//  RPG
+//
+//  Created by goggle CL on 2023/08/27.
+//
+
+import Foundation
+
+let buki = Buki(name: "ドラゴン", imageName: "monster.png", attackPoint: 10, maxHP: 300)
+
+let raizin = Enemy(name: "雷神", imageName: "boss_harmony.png", attackPoint: 10, maxHP: 300)
+
+let mahotukai = Enemy(name: "魔法使い", imageName: "boss_invader.png", attackPoint: 10, maxHP: 300)
+
+let yaritukai = Enemy(name: "槍使い", imageName: "boss_kane.png", attackPoint: 10, maxHP: 300)
+
+let mazin = Enemy(name: "魔神", imageName: "boss_manaman.ping", attackPoint: 10, maxHP: 300)
+
+let rich = Enemy(name: "金持ち", imageName: "boss_paragon.ping", attackPoint: 10, maxHP: 300)
