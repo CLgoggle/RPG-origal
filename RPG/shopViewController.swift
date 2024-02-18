4a0a149793c5844230b214fe07fd14bbc11af1ed
Clgoggle <misaryou.0404.0813@gmail.com>
2024-02-18 15:59:50 +0900

    カステラ

--- RPG/shopViewController.swift
+++ RPG/shopViewController.swift
@@ -1,16 +1,27 @@
 //
-//  bukisentakuViewController.swift
+//  shopViewController.swift
 //  RPG
 //
-//  Created by goggle CL on 2023/08/27.
+//  Created by goggle CL on 2024/02/04.
 //
 
 import UIKit
 
-class bukisentakuViewController: UIViewController {
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var nanidoLabel: UILabel!
+class shopViewController: UIViewController {
+    
+    @IBOutlet var nameLabel:UILabel!
+    @IBOutlet var imageView:UIImageView!
+    @IBOutlet var setumeiLabel:UILabel!
+    @IBOutlet var nedanLabel:UILabel!
+    @IBOutlet var MoneyLabel:UILabel!
+
+    let defaults = UserDefaults.standard
+    var index: Int = 0
+    
+    var Money = UserDefaults.standard.integer(forKey: "Money")
+    
+    
+
     
     var bukiArray: [String] = [
         "tsurugi_bronze_sabi_red","tsurugi_bronze_red","sword_shortsword_brown","sword_longsword_monochrome","sword_longsword_brown"
@@ -21,30 +32,24 @@ class bukisentakuViewController: UIViewController {
     
     var bukiattack: [Int] = [
     1,3,2,4,5]
+   
     
-    var nanidoArray: [String] = ["名のとおり錆びている剣。他の剣より攻撃力が低い",
-                                 "一般的な長さの剣",
-                                 "一般的なものより短い剣",
-                                 "一般的なものより長く、輝かしい剣",
-                                 "一般的なものより長い剣"]
+    var nedansutiArray: [Int] = [100,100,200,100,100]
+    var nedanArray: [String] = ["100円","100円","200円","100円","100円"]
     
-    var index: Int = 0
-    
-        
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-setUI()
+        setUI()
+        
+        MoneyLabel.text = "\(Money)円"
+
         // Do any additional setup after loading the view.
     }
-    
-   
     
     func setUI() {
         imageView.image = UIImage(named: bukiArray[index])
         nameLabel.text = nameArray[index]
-        nanidoLabel.text = nanidoArray[index]
+        nedanLabel.text = nedanArray[index]
     }
     
     @IBAction func next(){
@@ -68,20 +73,35 @@ setUI()
     }
     
     @IBAction func close(){
+        if Money >= nedansutiArray[index]{
+            Money -= nedansutiArray[index]
+        }
+        let Value = bukiArray[index]
+        defaults.set(Money, forKey: "Money")
+        UserDefaults.standard.set(Value, forKey: "buki")
+        MoneyLabel.text = "\(Money)円"
+        let Value2 = bukiattack[index]
+        UserDefaults.standard.set(Value2, forKey: "bukiattack")
+        if let savedValue = UserDefaults.standard.string(forKey: "buki"){
+            print(savedValue)
+        }
+    }
+    
+    
+    
+    @IBAction func close2(){
         let Value = bukiArray[index]
         UserDefaults.standard.set(Value, forKey: "buki")
         let Value2 = bukiattack[index]
         UserDefaults.standard.set(Value2, forKey: "bukiattack")
         if let savedValue = UserDefaults.standard.string(forKey: "buki"){
             print(savedValue)
+            
         }else {
             print("見つかりません")
         }
         dismiss(animated: true)
     }
-    
-  
-    
     
 
     /*
